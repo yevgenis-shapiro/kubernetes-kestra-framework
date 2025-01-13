a4a257c568471f660a8cb9b3f0dbc2459f08d2a3
Yevgeni Shapiro <yevgeni.shapiro@gmail.com>
2025-01-13 19:53:22 +0000

    update

---
+++ resources.tf
@@ -0,0 +1,89 @@
+
+data "external" "subnet" {
+  program = ["/bin/bash", "-c", "docker network inspect -f '{{json .IPAM.Config}}' kind | jq .[0]"]
+  depends_on = [
+    kind_cluster.default
+  ]
+}
+resource "helm_release" "metallb" {
+  name             = "metallb"
+  repository       = "https://metallb.github.io/metallb"
+  chart            = "metallb"
+  namespace        = "metallb"
+  version          = "0.10.3"
+  create_namespace = true
+  timeout = 300
+  values = [
+    <<-EOF
+  configInline:
+    address-pools:
+    - name: default
+      protocol: layer2
+      addresses:
+      - 172.18.255.1-172.18.255.250
+  EOF
+  ]
+  depends_on = [
+    kind_cluster.default
+  ]
+}
+
+resource "null_resource" "wait_for_metallb" {
+  triggers = {
+    key = uuid()
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      printf "\nWaiting for the metallb controller...\n"
+      kubectl wait --namespace ${helm_release.metallb.namespace} \
+        --for=condition=ready pod \
+        --selector=app.kubernetes.io/component=speaker \
+        --timeout=90s
+    EOF
+  }
+
+  depends_on = [helm_release.metallb]
+}
+
+
+resource "helm_release" "kestra" {
+  name             = "kestra"
+  chart            = "kestra"
+  repository       = "https://helm.kestra.io/"
+  namespace        = "kestra"
+  create_namespace = true
+  wait             = true
+  timeout          = 600
+
+  set {
+    name  = "deployments.webserver.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "deployments.executor.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "deployments.indexer.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "deployments.scheduler.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "deployments.worker.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "deployments.standalone.enabled"
+    value = "true"
+  }
+  depends_on = [helm_release.ingress_nginx]
+}
